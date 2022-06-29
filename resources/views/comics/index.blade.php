ef9cb6f17504c63506d580eb515ac09852e1995e
stefanocravotta <you@example.com>
2022-06-29 15:35:24 +0200

    add: Bootstrap - Model - Migration - Controller Comic

---
+++ resources/views/comics/index.blade.php
@@ -0,0 +1,8 @@
+@extends('layouts.main')
+
+@section('content')
+    <div class="container py-5">
+        <h1>Welcome To Comics Page</h1>
+        <h2>DC Comics</h2>
+    </div>
+@endsection
---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,13 @@
+<header>
+    <ul class="nav my-3">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="{{ route('home') }}">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="{{ route('comics.index') }}">Comics</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Link</a>
+        </li>
+      </ul>
+</header>
